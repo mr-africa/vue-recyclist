598b72e207416d98d0d784e3ddb590d1043588ec
Aleksei Efimov <aleksei@zservices-me.com>
2020-07-30 13:17:38 +0500

    linting

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    extends: [
+        '@vue/eslint-config-airbnb',
+        'plugin:vue/essential',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-param-reassign': 0,
+        indent: ['error', 4],
+        semi: [2, 'never'],
+        'space-before-function-paren': ['error', 'always'],
+        'import/no-named-as-default': 0,
+        'function-paren-newline': ['error', 'consistent'],
+        'vue/html-indent': 0,
+        'vue/attributes-order': 0,
+        'no-unused-expressions': 0, // temporary, because of optional-chaining
+        'vue/name-property-casing': ['error', 'kebab-case'],
+        'vue/max-attributes-per-line': [2, {
+            singleline: 1,
+            multiline: {
+                max: 1,
+                allowFirstLine: true,
+            },
+        }],
+        'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
+        'vue/no-use-v-if-with-v-for': 0,
+    },
+}
